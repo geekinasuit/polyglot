2616e738a025ae09e890ef6d50e8486f8394d593
Christian Gruber <christian@geekinasuit.com>
2025-02-18 16:12:22 -0800

    Implement and test bracket checking in go.

--- golang/brackets/brackets.go
+++ golang/brackets/brackets.go
@@ -1,5 +1,53 @@
 package brackets
 
+import (
+	"fmt"
+
+	"github.com/emirpasic/gods/stacks/linkedliststack"
+)
+
+var openParentheses = map[rune]rune{
+	'(': ')',
+	'{': '}',
+	'[': ']',
+}
+var closedParentheses = map[rune]rune{
+	')': '(',
+	'}': '{',
+	']': '[',
+}
+
 func BalancedBrackets(text string) error {
-	panic("Implement me")
+	if text == "" {
+		return nil
+	}
+	runes := []rune(text)
+	stack := linkedliststack.New()
+
+	for i, r := range runes {
+		if openParentheses[r] != 0 {
+			stack.Push(r)
+		} else if closedParentheses[r] != 0 {
+			open, ok := stack.Pop()
+			if !ok {
+				return fmt.Errorf("closing bracket %c with no opening bracket at rune %d", r, i+1)
+			}
+			if open != closedParentheses[r] {
+				return fmt.Errorf("closing bracket %c at rune %d mismatched with last opening bracket %c", r, i+1, open)
+			}
+		}
+	}
+	if !stack.Empty() {
+		stackStrings := "["
+		for i, v := range stack.Values() {
+			if i <= 0 {
+				stackStrings += fmt.Sprintf("%c", v)
+			} else {
+				stackStrings += fmt.Sprintf(", %c", v)
+			}
+		}
+		stackStrings += "]"
+		return fmt.Errorf("opening brackets without closing brackets found: %s", stackStrings)
+	}
+	return nil
 }
--- golang/brackets/brackets_test.go
+++ golang/brackets/brackets_test.go
@@ -1,11 +1,42 @@
 package brackets
 
 import (
-	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
-func TestFoo(t *testing.T) {
-	fmt.Println("blah")
-	panic("blah")
+func Test_Empty_Success(t *testing.T) {
+	err := BalancedBrackets("")
+	require.NoError(t, err)
+}
+
+func Test_SimpleBrackets_Success(t *testing.T) {
+	err := BalancedBrackets("()")
+	require.NoError(t, err)
+}
+
+func Test_SimpleBrackets_Fail_RemainingOpen(t *testing.T) {
+	err := BalancedBrackets("(()")
+	require.ErrorContains(t, err, "opening brackets without closing brackets found: [(]")
+}
+
+func Test_SimpleBrackets_Fail_ExtraClosed(t *testing.T) {
+	err := BalancedBrackets("(()))")
+	require.ErrorContains(t, err, "closing bracket ) with no opening bracket at rune 5")
+}
+
+func Test_SimpleBrackets_Fail_Mismatched(t *testing.T) {
+	err := BalancedBrackets("((])")
+	require.ErrorContains(t, err, "closing bracket ] at rune 3 mismatched with last opening bracket (")
+}
+
+func Test_ComplexText_Success(t *testing.T) {
+	err := BalancedBrackets("This is a bit of (albeit ridiculous) explanatory text. Don't (forget (to nest).).")
+	require.NoError(t, err)
+}
+
+func Test_ComplexText_Mismatched(t *testing.T) {
+	err := BalancedBrackets("This is a bit of (albeit ridiculous] explanatory text. Don't (forget (to nest).).")
+	require.ErrorContains(t, err, "closing bracket ] at rune 36 mismatched with last opening bracket (")
 }
