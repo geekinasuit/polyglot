46c16d055bd7d7d3444a6fd2f0b2d4e04937e4c2
Christian Gruber <christian@geekinasuit.com>
2025-02-18 16:53:38 -0800

    Rework a bit around the edges, moving the main file around, adding cobra. Leaving the actual command work to another commit, with a todo.

---
+++ golang/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/cgruber/brackets/golang/brackets"
+	"github.com/spf13/cobra"
+)
+
+var text string
+
+func init() {
+	text = "Hello, (world)!"
+}
+
+func main() {
+	if err := rootCmd.Execute(); err != nil {
+		_, _ = fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+var rootCmd = &cobra.Command{
+	Use:   "brackets",
+	Short: "Brackets checks for the use of balanced brackets in a piece of text.",
+	Run: func(cmd *cobra.Command, args []string) {
+		// TODO: Check input from stdin and file.
+		fmt.Printf("Checking text for brackets:\n%s\n", text)
+		err := brackets.BalancedBrackets(text)
+		if err != nil {
+			fmt.Printf("    Mismatched brackets in text: %v\n", err)
+			os.Exit(1)
+		} else {
+			fmt.Printf("    Ok - brackets match\n")
+		}
+	},
+}
